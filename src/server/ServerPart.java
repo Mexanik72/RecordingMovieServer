546f6209a3969d16b2f25ffa1790e1eff82d9823
SamoylovMihail <php-fan@yandex.ru>
2015-02-08 00:24:07 +0300

    Update ServerPart.java

--- src/server/ServerPart.java
+++ src/server/ServerPart.java
@@ -1,13 +1,16 @@
 package server;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class ServerPart {
@@ -20,48 +23,61 @@ public class ServerPart {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(200, 250);
         f.setLayout(new BorderLayout());
+        
         area = new JTextArea();
-        f.add(area, BorderLayout.PAGE_START);
-        //f.setAlwaysOnTop(true);
+        f.add(area, BorderLayout.CENTER);
+        
+        JScrollPane sp = new JScrollPane(area);  
+        
+        f.getContentPane().add(sp);
+        f.setAlwaysOnTop(true);
         f.setVisible(true);
+        area.setBackground(Color.LIGHT_GRAY);
         connect();
     }
    
-    public void connect(){
+    public void connect() {
         int port = 2154;
         
         try {
             ServerSocket ss = new ServerSocket(port);
-            area.append("Wait connect...");
-            jpb = new JProgressBar();
-            jpb.setIndeterminate(true);
-            f.add(jpb, BorderLayout.AFTER_LAST_LINE);
             
-            while(true){
+            while(true) {
+            	area.append("Ожидание подключения...\n");                
+            	jpb = new JProgressBar();
+                jpb.setIndeterminate(true);
+                jpb.setForeground(Color.black);
+                f.add(jpb, BorderLayout.AFTER_LAST_LINE);
                 Socket soket = ss.accept();
         
                 InputStream in = soket.getInputStream();
                 DataInputStream din = new DataInputStream(in);
             
-                area.setText("Передается файл\n");
+                area.append("Передается файл\n");
                 area.append("Прием нового файла: \n");
+                        
                 long fileSize = din.readLong(); // получаем размер файла
+                int fsaize = (int)(fileSize / (1024));
+                                
                 String fileName = din.readUTF(); //прием имени файла
                 area.append("Имя файла: " + fileName+"\n");
                 area.append("Размер файла: " + fileSize + " байт\n");
-                jpb.setMaximum((int)(fileSize / (1024)));
+                    
+                jpb.setMaximum(fsaize);
                 jpb.setIndeterminate(false);
-                
+            
                 byte[] buffer = new byte[64*1024];
                 FileOutputStream outF = new FileOutputStream(fileName);
                 int count, total = 0;
                     
                 while ((count = din.read(buffer)) != -1){               
-                    total += count;
+                	total += count;
                     outF.write(buffer, 0, count);
+                        
                     jpb.setValue(total/1024);
+                    
                     if(total == fileSize){
-                        break;
+                    	break;
                     }
                 }
                 outF.flush();
@@ -74,7 +90,8 @@ public class ServerPart {
             e.printStackTrace();
         }
     }
+    
     public static void main(String[] arg){
-        new ServerPart();
+    	new ServerPart();
     }
 }
