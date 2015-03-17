8977d4649928341e5add89d6654b2aaa56aaff97
SamoylovMihail <php-fan@yandex.ru>
2015-03-18 00:12:06 +0300

    Update ServerPart.java

--- server/src/server/server/ServerPart.java
+++ server/src/server/server/ServerPart.java
@@ -1,7 +1,5 @@
 package server;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -18,35 +16,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JProgressBar;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-
-
 public class ServerPart {
-	
-	private JTextArea area;
-	private JProgressBar jpb;
-	private JFrame f;
 
 	private DataInputStream din = null;
 	private DataOutputStream outD = null;
 	private Socket soket = null;
 
 	ServerPart() {
-		f = new JFrame("Server");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(200, 250);
-		f.setLayout(new BorderLayout());
-		area = new JTextArea();
-		f.add(area, BorderLayout.CENTER);
-		JScrollPane sp = new JScrollPane(area);
-		f.getContentPane().add(sp);
-		f.setAlwaysOnTop(true);
-		f.setVisible(true);
-		area.setBackground(Color.LIGHT_GRAY);
 		connect();
 	}
 
@@ -55,25 +31,18 @@ public class ServerPart {
 		try {
 			ServerSocket ss = new ServerSocket(port);
 			while (true) {
-				area.append("Ожидание подключения...\n");
-				jpb = new JProgressBar();
-				jpb.setIndeterminate(true);
-				jpb.setForeground(Color.black);
-				f.add(jpb, BorderLayout.AFTER_LAST_LINE);
 				soket = ss.accept();
 				InputStream in = soket.getInputStream();
 				din = new DataInputStream(in);
 				outD = new DataOutputStream(soket.getOutputStream());
-				area.append("Передается файл\n");
-				area.append("Прием нового файла: \n");
 				int flag = din.readInt();
 
 				if (flag == 0)
 					sendVideo();
-				else if (flag == 1)
-					sendButtonIcon();
-				else if (flag == 2)
-					sendUserIcon();
+//				else if (flag == 1)
+//					sendButtonIcon();
+//				else if (flag == 2)
+//					sendUserIcon();
 				else if (flag == 3)
 					getVideo();
 				else if (flag == 4)
@@ -105,16 +74,15 @@ public class ServerPart {
 				else if (flag == 17)
 					addRateToMovie();
 				else
-					throw new Exception();
-
+					break;
 			}
+			ss.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	private void getFiveTopUsers() throws Exception {
-		// TODO Auto-generated method stub
 		DataBaseScore dbs = new DataBaseScore();
 		List<Score> listScores = dbs.getFiveTopUser();
 		ObjectOutputStream out = new ObjectOutputStream(soket.getOutputStream());
@@ -123,15 +91,13 @@ public class ServerPart {
 	}
 
 	private void getScoreById() throws Exception {
-		// TODO Auto-generated method stub
 		DataBaseScore dbs = new DataBaseScore();
 		int userNow = din.readInt();
 		int scoreInt = dbs.getScoreByUser(userNow);
-		outD.writeInt(scoreInt);		
+		outD.writeInt(scoreInt);
 	}
 
 	private void getIdByWords() throws IOException {
-		// TODO Auto-generated method stub
 		Word wordNow = null;
 		DataBaseWord dw = new DataBaseWord();
 		String word = din.readUTF();
@@ -146,7 +112,6 @@ public class ServerPart {
 	}
 
 	private void getWordsWhereC() throws IOException {
-		// TODO Auto-generated method stub
 		DataBaseWord dw = new DataBaseWord();
 		ArrayList<String> Words = null;
 		int categoryNow = din.readInt();
@@ -154,7 +119,6 @@ public class ServerPart {
 		try {
 			Words = (ArrayList<String>) dw.getWordsWhereC(categoryNow);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -164,7 +128,6 @@ public class ServerPart {
 	}
 
 	private void addScoreAndMovie() throws IOException, ClassNotFoundException {
-		// TODO Auto-generated method stub
 		Movie mv = new Movie();
 		DataBaseMovies db = new DataBaseMovies();
 		
@@ -185,31 +148,27 @@ public class ServerPart {
 			java.sql.Date sqlDate = new java.sql.Date(someDate.getTime());
 			Score score = new Score();
 			score.setDate(sqlDate);
-			//score.setDate(new Date(System.currentTimeMillis()));
 			score.setUser(userNow.getId());
 			score.setRate(scoreNow);
 			DataBaseScore dbs1 = new DataBaseScore();
 			dbs1.addScore(score);
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		try {
 			db.addMovie(mv);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	private void getMoviesIds() throws IOException {
-		// TODO Auto-generated method stub
+		
 		DataBaseMovies dbmv = new DataBaseMovies();
 		List<Integer> ListIdmovies = null;
 		try {
 			ListIdmovies = dbmv.getMoviesIds();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		ObjectOutputStream out = new ObjectOutputStream(soket.getOutputStream());
@@ -218,7 +177,7 @@ public class ServerPart {
 	}
 
 	private void getMovieByCategor() throws IOException {
-		// TODO Auto-generated method stub
+		
 		DataBaseMovies dbm = new DataBaseMovies();
 		List<Movie> movies = new ArrayList<Movie>();
 		System.out.println("server is ok");
@@ -226,9 +185,7 @@ public class ServerPart {
         
 		try {
 			movies = dbm.getMovieByCategor(categor);
-			// words = (ArrayList<Integer>) dbw.getIdByCategories(categor);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -266,7 +223,6 @@ public class ServerPart {
 		 }
 	
 	private void addUser() throws ClassNotFoundException, IOException {
-		// TODO Auto-generated method stub
 		ObjectInputStream in = new ObjectInputStream(soket.getInputStream());
 		User user = (User) in.readObject();
 		in.close();
@@ -274,20 +230,17 @@ public class ServerPart {
 		try {
 			db.addUser(user);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
 	private void authentication() throws IOException {
-		// TODO Auto-generated method stub
 		DataBaseUsers db = new DataBaseUsers();
 		List<String> usersNames = null;
 		User User = new User();
 		try {
 			usersNames = db.getUsersNames();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		String user = din.readUTF();
@@ -301,7 +254,6 @@ public class ServerPart {
 				try {
 					User = db.getUserByUsername(usersNames.get(i));
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				if (User.getPassword().equals(pass)) {
@@ -321,20 +273,17 @@ public class ServerPart {
 	}
 
 	private void setImg() throws IOException {
-		// TODO Auto-generated method stub
 		DataBaseUsers db = new DataBaseUsers();
 		int id = din.readInt();
 		String img = din.readUTF();
 		try {
 			db.setImg(id, img);
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 	}
 
 	private void sendIdByCategories() throws Exception {
-		// TODO Auto-generated method stub
 		Categories categ;
 		DataBaseWord dw = new DataBaseWord();
 		String str = din.readUTF();
@@ -345,13 +294,11 @@ public class ServerPart {
 	}
 
 	private void sendCategories() throws IOException {
-		// TODO Auto-generated method stub
 		ArrayList<String> Categories = null;
 		DataBaseWord dw = new DataBaseWord();
 		try {
 			Categories = (ArrayList<String>) dw.getCategories();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		ObjectOutputStream out = new ObjectOutputStream(soket.getOutputStream());
@@ -360,43 +307,23 @@ public class ServerPart {
 	}
 
 	private void getVideo() throws IOException {
-		// TODO Auto-generated method stub
-		long fileSize = din.readLong(); // получаем размер файла
-		int fsaize = (int) (fileSize / (1024));
-		String fileName = din.readUTF(); // прием имени файла
-		area.append("Имя файла: " + fileName + "\n");
-		area.append("Размер файла: " + fileSize + " байт\n");
-		jpb.setMaximum(fsaize);
-		jpb.setIndeterminate(false);
+		long fileSize = din.readLong();
+		String fileName = din.readUTF();
 		byte[] buffer = new byte[64 * 1024];
 		FileOutputStream outF = new FileOutputStream(fileName);
 		int count, total = 0;
 		while ((count = din.read(buffer)) != -1) {
 			total += count;
 			outF.write(buffer, 0, count);
-			jpb.setValue(total / 1024);
 			if (total == fileSize) {
 				break;
 			}
 		}
 		outF.flush();
 		outF.close();
-		area.append("Файл принят\n---------------------------------\n");
-		jpb.setVisible(false);
-	}
-
-	private void sendUserIcon() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void sendButtonIcon() {
-		// TODO Auto-generated method stub
-
 	}
 
 	private void sendVideo() {
-		// TODO Auto-generated method stub
 		try {
 			String str = din.readUTF();
 			try {
@@ -418,7 +345,6 @@ public class ServerPart {
 				e.printStackTrace();
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
